ebe02effcbf24b0fc07d1094e21d530cc262d750
M-Phoenixx <mchuhan66@gmail.com>
2023-03-17 13:16:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import BmiCalculator from "./BmiCalculator";
+
+const App = () => {
+  return (
+    <div>
+      <BmiCalculator />
+    </div>
+  );
+};
+
+export default App;
